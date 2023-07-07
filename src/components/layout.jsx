25e75701d05d54b006b59b179cd6278326c206f0
Zahid Hasan <dihaz.nett@gmail.com>
2023-07-07 12:00:08 +0600

    ' '

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,12 +1,12 @@
 import TopMenu from './topMenu'
 import Footer from './footer'
 
-export default function Layout({children}) {
-    return (
-      <div className= " max-w-screen-xl mx-auto 3xl:max-w-screen-2xl flex flex-col justify-between">
-        <TopMenu color="white"/>
-        {children}
-        <Footer color="white"/>
-      </div>
-    )
-  }
+export default function Layout(props) {
+  return (
+    <div className={props.className + " max-w-screen-xl mx-auto 3xl:max-w-screen-2xl flex flex-col justify-between"}>
+      <TopMenu color="white" />
+      {props.children}
+      <Footer color="white" />
+    </div>
+  )
+}
