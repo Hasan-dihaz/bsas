c831e82d7ef24ccfe2c990566c1228686e53b22d
Zahid Hasan <dihaz.nett@gmail.com>
2023-07-10 09:53:04 +0600

    ' '

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,7 +6,7 @@ export default function About() {
                 <h1 className=" text-center pb-4">
                     About Us
                 </h1>
-                <p className=" text-base text-justify">
+                <p className=" text-base text-center">
                     BSAS was founded in XXXX to provide a full suite of services to companies. Our firm has over 30 years of combined experience in accounting, taxation, and other business services, and has now added capabilities in fund administration and management consulting. We have a team of 10 staff working seamlessly across two locations to support your business needs.
                 </p>
             </div>
